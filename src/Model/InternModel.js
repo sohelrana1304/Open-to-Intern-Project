89d0714c9564217665c4d2eb7bc3bacb78825be3
sonishinu <sk870221@gmail.com>
2022-05-05 14:34:48 +0530

    Api done

--- src/Model/InternModel.js
+++ src/Model/InternModel.js
@@ -1,36 +1,37 @@
-// const { Timestamp } = require('mongodb')
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-// const { ObjectId } = require('mongodb')
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
-const ObjectId = mongoose.Schema.Types.ObjectId
-
-const internSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        trim:true,
-        required:true
+const internSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
     },
-    email:{
-        type:String,
-        trim:true,
-        // unique:true,
-        required:true
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
     },
-    mobile:{
-        type:Number,
-        // unique:true,
-        required:true
+    mobile: {
+      type: Number,
+      unique: true,
+      trim: true,
+      required: true,
     },
-    collegeId:{
-        type:ObjectId,
-        ref:"college" //refred college collection
+    collegeId: {
+      type: ObjectId,
+      trim:true,
+      ref: "college", //refred college collection
     },
-    isDeleted:{
-        type:Boolean, default:false
-    }
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-}, { timestamps: true });
-
-
-module.exports = mongoose.model('intern', internSchema)
+module.exports = mongoose.model("intern", internSchema);
--- src/route/route.js
+++ src/route/route.js
@@ -2,8 +2,11 @@ const express = require('express')
 const router = express.Router();
 const collegeController = require('../Controller/CollegeController')
 const internController = require('../Controller/internController')
+//  const validator = require('../validator/validator')
 
-router.post("/functionup/colleges", collegeController.createclg)
+
+
+router.post("/functionup/colleges", collegeController.createCollege)
 
 router.post("/functionup/interns", internController.createIntern)
 
